10b2de6af7f25949b54cf3f0b638dfeb814441bc
Alvaro Hugo Garcia Cespedes <alvaro.garcia.c2022@gmail.com>
2022-07-21 16:11:12 -0400

    DC

--- layouts/PageSimulacro.jsx
+++ layouts/PageSimulacro.jsx
@@ -8,9 +8,9 @@ export default function PageSimulacro({ children }) {
       <Head>
         <title>Swoou Preuniversity: Banco de preguntas y Simulacro para examenes de admision</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="msapplication-navbutton-color" content="#000000" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="#000000" />
+        <meta name="theme-color" content="#15202B" />
+        <meta name="msapplication-navbutton-color" content="#15202B" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="#15202B" />
         <meta name="description" content="Desarrollando tecnología para hacer de un mundo complicado un mundo más sencillo" />
         <meta name="keywords" content="Swoou, Swoou preuniversity, banco de preguntas, cursos preuniversitarios, simulacro examen de admision" />
         <meta name="author" content="Raul Choque Romero" />
