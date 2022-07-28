c337ecd46e31e96cfff29b50e30976bd502483a3
Alvaro Hugo Garcia Cespedes <alvaro.garcia.c2022@gmail.com>
2022-07-27 22:53:43 -0400

    DC

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -6,6 +6,7 @@ export default function Loader() {
             <div className={style.spinner}>
                 <div></div>
                 <div></div>
+                <div></div>
             </div>
         </div>
 
