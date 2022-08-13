3a3fa6bc2262b70a0d712026225131dc3708e6f2
Alvaro Hugo Garcia Cespedes <alvaro.garcia.c2022@gmail.com>
2022-08-13 12:15:07 -0400

    DC

--- pages/Premium.jsx
+++ pages/Premium.jsx
@@ -1,46 +1,91 @@
 
-// import { useRouter } from 'next/router'
-// import Button from '../components/Button'
-// import PremiumC from '../components/PremiumC'
+import { useRouter } from 'next/router'
+import Button from '../components/Button'
+import PremiumC from '../components/PremiumC'
 // import PremiumPluss from '../components/PremiumPluss'
-import PageLayout from '../layouts/PageLayout'
 // import { getCode } from '../firebase/utils'
-// import { useUser } from '../context/Context.js'
+import { useUser } from '../context/Context.js'
+import PageUserLayout from '../layouts/PageUserLayout'
+
 import { WithAuth } from '../HOCs/WithAuth'
 // import Success from '../components/Success'
-// import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 // import Error from '../components/Error'
 // import Modal from '../components/Modal'
-// import style from '../styles/Premium.module.css'
+import style from '../styles/Premium.module.css'
 
 function Premium() {
-    // const router = useRouter()
-    // const [mode, setMode] = useState(false)
-    // const { user, userDB, setUserSuccess, success } = useUser()
-    // function x() {
-    //     setMode(!mode)
-    // }
-    // function nextClick(e) {
-    //     e.preventDefault()
-    //     const code = e.target.form[0].value
-    //     getCode(code, user.uid, setUserSuccess, userDB.profesor)
-    // }
-    // function backClick(e) {
-    //     e.preventDefault()
-    //     router.back()
-    // }
-    // function next() {
-    //     router.push("https://api.whatsapp.com/send?phone=+59173447725&text=Buenas,%20me%20gustaria%20adquirir%20Swoou%20Premium...%20%20")
-    // }
+    const router = useRouter()
+    const [mode, setMode] = useState(false)
+    const { user, userDB, setUserSuccess, success } = useUser()
+    function x() {
+        setMode(!mode)
+    }
+    function nextClick(e) {
+        e.preventDefault()
+        const code = e.target.form[0].value
+        getCode(code, user.uid, setUserSuccess, userDB.profesor)
+    }
+    function backClick(e) {
+        e.preventDefault()
+        router.back()
+    }
+    function next() {
+        router.push("https://api.whatsapp.com/send?phone=+59173447725&text=Buenas,%20me%20gustaria%20adquirir%20Swoou%20Premium...%20%20")
+    }
 
-    // function enlace() {
-    //     router.push("/Policy")
-    // }
+    function enlace() {
+        router.push("/Policy")
+    }
 
     return (
-        <>
-            <PageLayout>
-                {/* {userDB.premium !== null && <div className={style.container}>
+
+        <PageUserLayout>
+            <div className={style.container}>
+                <PremiumC></PremiumC>
+
+                <ul className={style.list}>
+                    <li className={style.li}>Simulaco ilimitado <img src='/right.svg' className={style.right} alt='rigth'></img></li>
+                    <li className={style.li}>Robot ilimitado <img src='/right.svg' className={style.right} alt='rigth'></img></li>
+                    <li className={style.li}>Cuadernillo de practica pdf <img src='/right.svg' className={style.right} alt='rigth'></img></li>
+                    <li className={style.li}>No publicidad <img src='/right.svg' className={style.right} alt='rigth'></img></li>
+                    <li className={style.li}>Soporte Tecnico <img src='/right.svg' className={style.right} alt='rigth'></img></li>
+                </ul>
+
+                {userDB.premium === false &&
+                    <>
+                        <div className={style.buttonsContainer}>
+                            <Button style='buttonGetPremium' click={backClick}><span className={style.buttonContent}><span>Trimestre 27 BS</span><span>Adquirirlo ya</span></span></Button>
+                            <Button style='buttonGetPremium' click={backClick}><span className={style.buttonContent}><span>Semestre 37 BS</span><span>Adquirirlo ya</span></span></Button>
+                            <Button style='buttonGetPremium' click={backClick}><span className={style.buttonContent}><span>Año 47 BS</span><span>Adquirirlo ya</span></span></Button>
+                            <Button style='buttonPrimary' click={backClick}>Ya cuento con un codigo Swoou Premium</Button>
+                            <div>
+                                <a className={style.enlace} onClick={enlace}>Terminos y condiciones Swoou Premium</a> <br />
+                            </div>
+                        </div>
+                    </>
+                }
+
+
+
+
+
+
+            </div>
+        </PageUserLayout>
+    )
+}
+export default WithAuth(Premium)
+
+
+
+
+
+
+
+
+
+            {/* {userDB.premium !== null && <div className={style.container}>
 
                     <PremiumC></PremiumC>
                     {userDB.premium === false && <div className={style.tiket}>9.70 BOB</div>}
@@ -76,7 +121,7 @@ function Premium() {
                         </div>}
                 </div>} */}
 
-            </PageLayout>
+     
             {/* <Modal mode={mode} click={x} text={'Ingresa tu codigo de activación'}>
                 <form className={style.form}>
                     <input className={style.input} type="text" placeholder='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx' />
@@ -85,8 +130,5 @@ function Premium() {
             </Modal>
             {success == true && <Success>Correcto</Success>}
             {success == false && <Error>Error</Error>} */}
-        </>
-    )
-}
-
-export default WithAuth(Premium)
+  
+        
